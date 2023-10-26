1662ec6114742843f00021f0f2f89742bf19193d
Ilya Marin <72019262+imarin73@users.noreply.github.com>
2023-10-26 10:27:28 +0400

    delete trash :)

--- controller/flights.js
+++ controller/flights.js
@@ -51,5 +51,51 @@ class Fligths {
             return response.json(result.rows);
         }
     }
+
+    async getDelayedByAirport(request, response) {
+
+  
+    //Получаем количество задержанных рейсов и время задержки в минутах по каждому аэропорту
+    `SELECT EXTRACT(DOW FROM scheduled_departure::timestamp) AS day_of_week,
+    EXTRACT(EPOCH FROM (scheduled_departure::timestamp - actual_departure::timestamp)) / 60 AS difference_in_minutes
+    FROM flights f 
+    WHERE scheduled_departure <> actual_departure
+    and departure_airport = 'KZN'
+    ORDER BY day_of_week;`
+
+    //Получаем колечество и время задержанных рейсов по дню недели
+    `
+    SELECT EXTRACT(DOW FROM scheduled_departure::timestamp) AS day_of_week,
+       EXTRACT(EPOCH FROM (scheduled_departure::timestamp - actual_departure::timestamp)) / 60 AS difference_in_minutes
+        FROM flights f
+        WHERE scheduled_departure <> actual_departure
+        AND departure_airport = 'KZN'
+        AND EXTRACT(DOW FROM scheduled_departure::timestamp) = 1 -- Замените 1 на нужный вам день недели (от 0 до 6)
+        ORDER BY day_of_week;
+    `
+    const arr = ['REN', 'KXK', 'IKT', 'TBW', 'AER', 'NBC',
+      'KRR', 'PKC', 'EGO', 'NYA', 'KLF', 'VKT', 'GOJ', 'GRV', 'RTW', 'URS', 'YKS',
+      'UUS', 'VOG', 'LED', 'OGZ', 'SLY', 'NJC', 'UKX', 'MCX', 'KUF', 'CEE', 'OSW', 'UIK',
+      'UUD', 'MQF', 'KJA', 'KGD', 'BZK', 'CNN', 'IAR', 'KZN', 'SVX', 'USK', 'ABA',
+      'BQS', 'UUA', 'PEZ', 'SWT', 'MJZ', 'MMK', 'KVX', 'ULY', 'DME', 'MRV', 'ARH',
+      'RGK', 'PEE', 'PYJ', 'BTK', 'CSY', 'GDZ', 'KRO', 'EYK', 'OMS', 'ROV', 'UCT',
+      'VOZ', 'PES', 'ULV', 'DYR', 'KYZ', 'SCW', 'VKO', 'URJ', 'LPK', 'BAX', 'KEJ',
+      'ASF', 'VVO', 'KHV', 'NUX', 'TOF', 'NNM', 'NFG', 'AAQ', 'OVB', 'NYM', 'STW',
+      'TJM', 'KGP', 'GDX', 'OVS', 'UFA', 'SVO', 'IJK', 'HTA', 'IWA', 'NOJ', 'JOK',
+      'SKX', 'HMA', 'NOZ', 'NAL', 'ESL', 'NSK', 'SGC', 'PKV', 'CEK']
+
+    }
+    async getDelayedByAirportAndDay(request, response) {
+            //Получаем колечество и время задержанных рейсов по дню недели
+    `
+    SELECT EXTRACT(DOW FROM scheduled_departure::timestamp) AS day_of_week,
+       EXTRACT(EPOCH FROM (scheduled_departure::timestamp - actual_departure::timestamp)) / 60 AS difference_in_minutes
+        FROM flights f
+        WHERE scheduled_departure <> actual_departure
+        AND departure_airport = 'KZN'
+        AND EXTRACT(DOW FROM scheduled_departure::timestamp) = 1 -- Замените 1 на нужный вам день недели (от 0 до 6)
+        ORDER BY day_of_week;
+    `
+    }
 }
 module.exports = new Fligths();
